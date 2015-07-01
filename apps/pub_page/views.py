60a4cb91d13b85c804a2f7e341677e2cb1a2d875
Allen-yan <wentao1988@gmail.com>
2015-07-02 00:20:56 +0800

    首页动态内容更新内容

--- apps/pub_page/views.py
+++ apps/pub_page/views.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import json
 
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from django.shortcuts import render, render_to_response, get_object_or_404, HttpResponseRedirect
@@ -12,13 +11,21 @@ def json_default_processor(obj):
         return vars(obj)
 
 def index(request):
-    return render(request, 'pub_page/index.html')
+    show_count = 5
+    data = {
+        "reading_classes": models.Article.objects.filter(article_type='1')[:show_count],
+        "train_classes": models.Article.objects.filter(article_type='2')[:show_count],
+        "reading_show": models.Article.objects.filter(article_type='3')[:show_count],
+        "feelings": models.Article.objects.filter(article_type='4')[:show_count],
+    }
+    return render_to_response('pub_page/index.html', data)
 
 def intro(request):
     return render(request, 'pub_page/intro.html')
 
 
 def news(request):
+    # news: article type 5
     news_page_list = get_articles_page_list('5', request.GET.get("page", 1))
 
     return render_to_response(
@@ -49,18 +56,6 @@ def get_articles_page_list(article_type, selected_page=1):
         page_art = paginator.page(1)
 
     return page_art
-
-
-def article_detail(request, article_id):
-    article = get_object_or_404(models.Article, id=article_id)
-
-    if not article:
-        raise
-    data = {
-        "article": vars(article)
-    }
-
-    return render_to_response("pub_page/article_detail.html", data)
 
 
 def feelings(request):
